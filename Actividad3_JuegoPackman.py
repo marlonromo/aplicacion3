f15590a5f4d83fc6b3b9a4ae8193296a81cea620
Marlon Romo <marlon.romolpz@gmail.com>
2021-06-29 12:10:08 -0500

    Update Actividad3_JuegoPackman.py

--- Actividad3_JuegoPackman.py
+++ Actividad3_JuegoPackman.py
@@ -58,6 +58,56 @@ def world():
 
 def move():
     "Move pacman and all ghosts."
+<<<<<<< Updated upstream
+=======
+    "Move pacman and all ghosts."
+    writer.undo()
+    writer.write(state['score'])
+
+    clear()
+
+    if valid(pacman + aim):
+        pacman.move(aim)
+
+    index = offset(pacman)
+
+    if tiles[index] == 1:
+        tiles[index] = 2
+        state['score'] += 1
+        x = (index % 20) * 20 - 200
+        y = 180 - (index // 20) * 20
+        square(x, y)
+
+    up()
+    goto(pacman.x + 10, pacman.y + 10)
+    dot(20, 'yellow')
+
+    for point, course in ghosts:
+        if valid(point + course):
+            point.move(course)
+        else:
+            options = [
+                vector(5, 0),
+                vector(-5, 0),
+                vector(0, 5),
+                vector(0, -5),
+            ]
+            plan = choice(options)
+            course.x = plan.x
+            course.y = plan.y
+
+        up()
+        goto(point.x + 10, point.y + 10)
+        dot(20, 'red')
+
+    update()
+
+    for point, course in ghosts:
+        if abs(pacman - point) < 20:
+            return
+
+    ontimer(move, 100)
+>>>>>>> Stashed changes
 
 
 def change(x, y):
