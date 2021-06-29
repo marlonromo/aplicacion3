d5ddc801b52cc67437a1a70b87748ae522ccc105
Marlon Romo <marlon.romolpz@gmail.com>
2021-06-29 11:49:53 -0500

    Agregar Funcion Move()
    Se agrego la funcion move correctamente

--- Actividad2_JuegoVibora.py
+++ Actividad2_JuegoVibora.py
@@ -18,6 +18,32 @@ def inside(head):
 
 def move():
     "Move snake forward one segment."
+    "Move snake forward one segment."
+    head = snake[-1].copy()
+    head.move(aim)
+
+    if not inside(head) or head in snake:
+        square(head.x, head.y, 9, 'red')
+        update()
+        return
+
+    snake.append(head)
+
+    if head == food:
+        print('Snake:', len(snake))
+        food.x = randrange(-15, 15) * 10
+        food.y = randrange(-15, 15) * 10
+    else:
+        snake.pop(0)
+
+    clear()
+
+    for body in snake:
+        square(body.x, body.y, 9, 'black')
+
+    square(food.x, food.y, 9, 'green')
+    update()
+    ontimer(move, 100)
 
 
 setup(420, 420, 370, 0)
