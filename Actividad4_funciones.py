674f96d967e56a8643a602e88afdd9489c6a5734
Marlon Romo <marlon.romolpz@gmail.com>
2021-06-29 12:14:59 -0500

    Create Actividad4_funciones.py

---
+++ Actividad4_funciones.py
@@ -0,0 +1,33 @@
+from random import randrange
+from turtle import *
+
+from freegames import vector
+
+ball = vector(-200, -200)
+speed = vector(0, 0)
+targets = []
+
+
+def tap(x, y):
+    "Respond to screen tap."
+
+
+def inside(xy):
+    "Return True if xy within screen."
+
+
+def draw():
+    "Draw ball and targets."
+
+
+def move():
+    "Move ball and targets."
+
+
+setup(420, 420, 370, 0)
+hideturtle()
+up()
+tracer(False)
+onscreenclick(tap)
+move()
+done()
