98fcba696fc1949c80c8a8400450ae073d14aa20
Marlon Romo <marlon.romolpz@gmail.com>
2021-06-29 11:43:39 -0500

    Create Actividad2_JuegoVibora.py
    Programa Actividad 2 incompleto

---
+++ Actividad2_JuegoVibora.py
@@ -0,0 +1,32 @@
+from random import randrange
+from turtle import *
+
+from freegames import square, vector
+
+food = vector(0, 0)
+snake = [vector(10, 0)]
+aim = vector(0, -10)
+
+
+def change(x, y):
+    "Change snake direction."
+
+
+def inside(head):
+    "Return True if head inside boundaries."
+
+
+def move():
+    "Move snake forward one segment."
+
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+listen()
+onkey(lambda: change(10, 0), 'Right')
+onkey(lambda: change(-10, 0), 'Left')
+onkey(lambda: change(0, 10), 'Up')
+onkey(lambda: change(0, -10), 'Down')
+move()
+done()
