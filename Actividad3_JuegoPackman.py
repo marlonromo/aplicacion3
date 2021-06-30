2aabc31bff05524707f52cdd702824aac7fb580a
A00827878 <A00827878@itesm.mx>
2021-06-30 09:33:38 -0500

    funcion world
    agregar funcion world

--- Actividad3_JuegoPackman.py
+++ Actividad3_JuegoPackman.py
@@ -54,6 +54,21 @@ def valid(point):
 
 def world():
     "Draw world using path."
+ bgcolor('black')
+    path.color('blue')
+
+    for index in range(len(tiles)):
+        tile = tiles[index]
+
+        if tile > 0:
+            x = (index % 20) * 20 - 200
+            y = 180 - (index // 20) * 20
+            square(x, y)
+
+            if tile == 1:
+                path.up()
+                path.goto(x + 10, y + 10)
+                path.dot(2, 'white')
 
 
 def move():
