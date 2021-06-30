0caa874069799a86aeeb1450c0f0bd7dd1adeefb
FranciscoAmbriz <a01379117@itesm.mx>
2021-06-30 09:51:11 -0500

    Create Actividad4_TiroParabolico.py
    Se creo el programa act4

---
+++ Actividad4_TiroParabolico.py
@@ -0,0 +1,31 @@
+from random import randrange
+from turtle import *
+
+from freegames import vector
+
+ball = vector(-200, -200)
+speed = vector(0, 0)
+targets = []
+
+
+def tap(x, y):
+ 
+
+
+def inside(xy):
+ 
+
+
+def draw():
+  
+def move():
+
+
+
+setup(420, 420, 370, 0)
+hideturtle()
+up()
+tracer(False)
+onscreenclick(tap)
+move()
+done()
